cd93eae03e99f719e8905315c1286866b50d45fb
Gildo Siqueira <gildo_siqueira@hotmail.com>
2015-06-14 21:25:09 -0300

    Classes de paginação

---
+++ Gildo.lojaVirtual.Web/Controllers/VitrineController.cs
@@ -0,0 +1,25 @@
+﻿using Gildo.LojaVirtual.Dominio.Repositorio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gildo.lojaVirtual.Web.Controllers
+{
+    public class VitrineController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+        public int ProdutosPorPagina = 9;
+        public ActionResult ListaProdutos(int pagina = 1)
+        {
+            _repositorio = new ProdutosRepositorio();
+            var produtos = _repositorio.Produtos
+                .OrderBy(p => p.Descricao)
+                .Skip((pagina - 1) * ProdutosPorPagina)
+                .Take(ProdutosPorPagina);
+
+            return View(produtos);
+        }
+	}
+}
---
+++ Gildo.lojaVirtual.Web/HtmlHelpers/PaginacaoHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web.Mvc;
+using Gildo.lojaVirtual.Web.Models;
+using System.Text;
+
+
+namespace Gildo.lojaVirtual.Web.HtmlHelpers
+{
+    public static class PaginacaoHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html, Paginacao pagincao, Func<int, string> paginaUrl)
+        {
+            StringBuilder resultado = new StringBuilder();
+
+            for (int i = 1 ; i < pagincao.TotalPagina ; i ++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", paginaUrl(i));
+                tag.InnerHtml = i.ToString();
+
+                if (i == pagincao.PaginaAtual)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+
+                tag.AddCssClass("btn-default");
+                resultado.Append(tag);
+            }
+
+            return MvcHtmlString.Create(resultado.ToString());            
+        }
+    }
+}
